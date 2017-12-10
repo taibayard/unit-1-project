6c03a76f2f40cf92395fdea7a01e7f751ca29de4
Admin <admin@Admins-MacBook-Pro.local>
2017-12-09 23:14:40 -0800

    added click value upgrade button. Added algorithm to set cost/value of click obj.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const coin = document.getElementsByClassName("bitcoin-wrapper")[0];
+/*stats*/
 const clickCount = document.getElementsByClassName("click-count")[0].getElementsByTagName("a")[0];
 const cashLabel = document.getElementsByClassName("current-cash")[0].getElementsByTagName("a")[0];
 const clickProfitLabel = document.getElementsByClassName("click-profit")[0].getElementsByTagName("a")[0];
+const totalProfitLabel = document.getElementsByClassName("total-profit")[0].getElementsByTagName("a")[0];
+/*upgrades*/
+const perClickUpgrade = document.getElementsByClassName("per-click-upgrade")[0];
+/*game data*/
 let roundOffset = 3;
 let game = {
     income: {
@@ -13,8 +18,20 @@ let game = {
         clicks: 0,
         cashFromClicks: 0,
         totalCashGenerated: 0
+    },
+    upgrades: {
+        perClick: {
+            total: 0,
+            cost: 0.005,
+            value: 0.001
+        }
     }
 }
+
+function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+
 var coinClick = function() {
     game.income.cash += (game.income.clickWorth * game.income.clickMultiplier)
     game.stats.cashFromClicks += (game.income.clickWorth * game.income.clickMultiplier);
@@ -22,17 +39,47 @@ var coinClick = function() {
     game.stats.clicks++;
     setLocalStorage();
 }
+var upgradeClick = function(u, el) {
+    console.log("clicked upgrades");
+    let upgrade = game.upgrades[u];
+    upgrade.total++;
+    upgrade.cost += upgrade.cost * 2.3;
+    upgrade.cost = round(upgrade.cost, roundOffset);
+    upgrade.value += upgrade.value *0.1;
+    upgrade.value = round(upgrade.value,roundOffset + 1);
+    el.getElementsByClassName("upgrade-cost")[0].innerText = upgrade.cost;
+    el.getElementsByTagName("a")[0].innerText = upgrade.total;
+    return upgrade;
+}
+
+function setClickAnimation(e) {
+    e.style.setProperty("transform", "scale(1.1)");
+}
+
+function resetClickAnimation(e) {
+    e.style.setProperty("transform", "scale(1)");
+}
 
 function addEvents() {
     coin.addEventListener("mousedown", function() {
-        this.style.setProperty("transform", "scale(1.1)");
+        setClickAnimation(this);
         coinClick();
     })
     coin.addEventListener("mouseup", function() {
-        this.style.setProperty("transform", "scale(1)");
+        resetClickAnimation(this);
+    })
+    perClickUpgrade.addEventListener("click", function() {
+        let diff = round((game.income.cash - game.upgrades.perClick.cost), roundOffset)
+        if (diff >= 0) {
+            game.upgrades.perClick = upgradeClick("perClick", this);
+            game.income.cash = diff;
+            game.income.clickWorth += game.upgrades.perClick.value;
+            console.log(game.upgrades.perClick.value);
+            setLocalStorage();
+        }
     })
 }
 window.onload = function() {
-    setLocalStorage();
+    loadLocalStorage();
     addEvents();
 }
--- js/storage.js
+++ js/storage.js
@@ -1,4 +1,4 @@
-var storageHandler  = function(gameObj, storageObj) {
+var storageHandler = function(gameObj, storageObj) {
     let z = parseFloat(localStorage.getItem(storageObj));
     switch (true) {
         case isNaN(z):
@@ -8,7 +8,7 @@ var storageHandler  = function(gameObj, storageObj) {
             localStorage.setItem(storageObj, gameObj);
             break;
         case gameObj < z:
-            return z;
+            return gameObj;
             break;
         default:
             console.log("error");
@@ -16,21 +16,39 @@ var storageHandler  = function(gameObj, storageObj) {
     }
     return gameObj;
 }
+
+function loadLocalStorage() {
+    if (localStorage.length > 0) {
+        console.log("working");
+        game.income.cash = localStorage.getItem("currentCash");
+        game.stats.cashFromClicks = localStorage.getItem("cashFromClicks");
+        game.stats.totalCashGenerated = localStorage.getItem("totalEarnedCash");
+        game.stats.clicks = localStorage.getItem("totalClicks");
+    }
+    setLocalStorage();
+
+}
+
 function setLocalStorage() {
     //handle storage for clicks
     handleClickStorage();
     //handle storage for cash
     handleCashStorage();
 }
+
 function handleCashStorage() {
     //handling current cash
-    game.income.cash = parseFloat((game.income.cash).toFixed(roundOffset));
+    game.income.cash = round(parseFloat(game.income.cash),roundOffset);
     game.income.cash = storageHandler(game.income.cash, "currentCash");
     cashLabel.innerText = game.income.cash;
     //handles memeory for profit from click history
-    game.stats.cashFromClicks = parseFloat((game.stats.cashFromClicks).toFixed(roundOffset));
+    game.stats.cashFromClicks = round(parseFloat(game.stats.cashFromClicks),roundOffset);
     game.stats.cashFromClicks = storageHandler(game.stats.cashFromClicks, "cashFromClicks");
     clickProfitLabel.innerText = game.stats.cashFromClicks;
+    //handles memory for total earned
+    game.stats.totalCashGenerated = round(parseFloat(game.stats.totalCashGenerated),roundOffset);
+    game.stats.totalCashGenerated = storageHandler(game.stats.totalCashGenerated, "totalEarnedCash");
+    totalProfitLabel.innerText = game.stats.cashFromClicks;
 }
 
 function handleClickStorage() {
