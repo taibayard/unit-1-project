64997e9d4eed7a978157f860eb0c53b4ea248fdc
Admin <admin@Admins-MacBook-Pro.local>
2017-12-12 11:55:49 -0800

    added timer offset to smooth out how upgrades get added to total

--- js/btc-per-second.js
+++ js/btc-per-second.js
@@ -1,10 +1,12 @@
+var timeOffset = 10;
+var timr = 1000/timeOffset;
 function addBtc(){
 	setInterval(function(){
 		let z = game.income.cash;
-		game.income.cash += game.upgrades.values.gpu;
-		game.stats.totalCashGenerated += game.upgrades.values.gpu;
+		game.income.cash += game.upgrades.values.gpu/timeOffset;
+		game.stats.totalCashGenerated += game.upgrades.values.gpu/timeOffset;
 		if(z != game.income.cash && isNaN(z) === false && z != null && localStorage.length > 0){
 			setLocalStorage();
 		}
-	},1000)
+	},timr)
 }
